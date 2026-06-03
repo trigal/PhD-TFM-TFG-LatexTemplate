2e8a15de64d84d0307f2542a823677f76486c127
Augusto Luis Ballardini <trigal@gmail.com>
2026-06-03 12:43:10 +0200

    Delete AdminScripts directory

--- AdminScripts/getAllBranches.sh
+++
@@ -1,7 +0,0 @@
-for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `
-do
-   CMD="git branch --track ${branch#remotes/origin/} $branch"
-   echo "Running [$CMD]"
-   $CMD
-done
-
